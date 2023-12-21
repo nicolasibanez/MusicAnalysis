34bf05b949a309f1f0a13713a3cfe81e49b15d70
Nicolas Ibanez <nicolas.ibanez@student-cs.fr>
2023-12-21 22:23:03 +0100

    first commit

---
+++ install_set.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+
+def main():
+    path = 'https://drive.google.com/file/d/19xXeKczn_g_QShj4FJdaorJOzJOz0Yxa/view?usp=sharing'
+    # download the file
+    os.system('wget ' + path)
+    # unzip the file
+    # name : podcast001.tar
+    os.system('tar -xzvf podcast001.tar')
+
+
+
+if __name__ == '__main__':
+    main()
